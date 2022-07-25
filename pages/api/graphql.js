eb098943e66b78763cba5ab867629fba6db7de8e
SiTribal <simon.colman@au.tribalworldwide.com>
2022-07-25 11:27:35 +1000

    initial commit

---
+++ pages/api/graphql.js
@@ -0,0 +1,61 @@
+import Cors from 'micro-cors'
+import {gql, ApolloServer} from 'apollo-server-micro'
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+const books = [ 
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin'
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster'
+    },
+]
+
+
+const typeDefs = gql`
+    type Book {
+        title: String
+        author: String
+    }
+
+    type Query{
+        books: [Book]
+    }
+`
+
+const resolvers = {
+    Query: {
+        books: () => books
+    }
+}
+
+const cors = Cors()
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({req}) =>{
+
+    },
+    introspection: true,
+    playground: true,
+});
+
+const serverStart = apolloServer.start()
+
+export default cors(async(req, res) => {
+    if(req.method === 'OPTIONS'){
+        res.end();
+        return false;
+    }
+
+    await serverStart
+    await apolloServer.createHandler({path: '/api/graphql'})(req,res);
+})
